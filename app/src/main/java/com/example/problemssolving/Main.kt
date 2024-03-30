346849ece6aef97c9a6eb8b7ba3f3f35718fb61e
mohamedalaa4j <mohamedalaa4j@gmail.com>
2024-03-30 15:32:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/problemssolving/Main.kt
@@ -0,0 +1,10 @@
+package com.example.problemssolving
+
+import com.example.problemssolving.problems.kyu8.EvenOrOdd
+
+fun main() {
+    println(
+//        ReplaceVowelToExclamationMark().replace("Hi!")
+        EvenOrOdd().evenOrOdd(-12)
+    )
+}
