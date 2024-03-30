94b8a039e52f8b414100943b7783a78958191391
mohamedalaa4j <mohamedalaa4j@gmail.com>
2024-03-30 18:14:45 +0200

    isPalindromeNumber - leetCode

--- app/src/main/java/com/example/problemssolving/Main.kt
+++ app/src/main/java/com/example/problemssolving/Main.kt
@@ -1,11 +1,13 @@
 package com.example.problemssolving
 
 import com.example.problemssolving.problems.leetcode.DefangIpAddress
+import com.example.problemssolving.problems.leetcode.PalindromeNumber
 
 fun main() {
     println(
 //        ReplaceVowelToExclamationMark().replace("Hi!")
 //        EvenOrOdd().evenOrOdd(-12)
-        DefangIpAddress().defangIpAddress("255.100.50.0")
+//        DefangIpAddress().defangIpAddress("255.100.50.0")
+        PalindromeNumber().isPalindromeNumber(-121)
     )
 }
---
+++ app/src/main/java/com/example/problemssolving/problems/leetcode/PalindromeNumber.kt
@@ -0,0 +1,18 @@
+package com.example.problemssolving.problems.leetcode
+
+class PalindromeNumber {
+
+    fun isPalindromeNumber(number: Int): Boolean {
+        val input = number.toString()
+        var reversedInput = ""
+
+        for (i in input.length - 1 downTo 0) {
+            reversedInput += input[i]
+        }
+        return  input == reversedInput
+    }
+
+    fun isPalindromeNumber2(number: Int):Boolean{
+        return  number.toString() == number.toString().reversed()
+    }
+}
