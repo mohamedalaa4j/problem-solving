415daed2ad36edd77634f69f63e7d33c614ed70b
mohamedalaa4j <mohamedalaa4j@gmail.com>
2024-03-30 15:32:50 +0200

    EvenOrOdd

---
+++ app/src/main/java/com/example/problemssolving/problems/kyu8/EvenOrOdd.kt
@@ -0,0 +1,12 @@
+package com.example.problemssolving.problems.kyu8
+
+/*
+Create a function that takes an integer as an argument
+and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+class EvenOrOdd {
+    fun evenOrOdd(number: Int): String {
+        return if (number % 2 == 0) "Even" else "Odd"
+    }
+}
